a605e229ba5ddf6c30c06ef1364cd2713d552850
rhogan@broadwaymedia.com <rhogan7@gmail.com>
2023-01-03 17:42:04 -0800

    Hid typescript errors.

--- apps/daoism/pages/index.tsx
+++ apps/daoism/pages/index.tsx
@@ -11,83 +11,83 @@ import 'keen-slider/keen-slider.min.css';
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
-function ThumbnailPlugin(
-  mainRef: MutableRefObject<KeenSliderInstance | null>
-): KeenSliderPlugin {
-  return (slider) => {
-    function removeActive() {
-      slider.slides.forEach((slide) => {
-        slide.classList.remove('active');
-      });
-    }
-    function addActive(idx: number) {
-      slider.slides[idx].classList.add('active');
-    }
+// function ThumbnailPlugin(
+//   mainRef: MutableRefObject<KeenSliderInstance | null>
+// ): KeenSliderPlugin {
+//   return (slider) => {
+//     function removeActive() {
+//       slider.slides.forEach((slide) => {
+//         slide.classList.remove('active');
+//       });
+//     }
+//     function addActive(idx: number) {
+//       slider.slides[idx].classList.add('active');
+//     }
 
-    function addClickEvents() {
-      slider.slides.forEach((slide, idx) => {
-        slide.addEventListener('click', () => {
-          if (mainRef.current) mainRef.current.moveToIdx(idx);
-        });
-      });
-    }
+//     function addClickEvents() {
+//       slider.slides.forEach((slide, idx) => {
+//         slide.addEventListener('click', () => {
+//           if (mainRef.current) mainRef.current.moveToIdx(idx);
+//         });
+//       });
+//     }
 
-    slider.on('created', () => {
-      if (!mainRef.current) return;
-      addActive(slider.track.details.rel);
-      addClickEvents();
-      mainRef.current.on('animationStarted', (main) => {
-        removeActive();
-        const next = main.animator.targetIdx || 0;
-        addActive(main.track.absToRel(next));
-        slider.moveToIdx(Math.min(slider.track.details.maxIdx, next));
-      });
-    });
-  };
-}
+//     slider.on('created', () => {
+//       if (!mainRef.current) return;
+//       addActive(slider.track.details.rel);
+//       addClickEvents();
+//       mainRef.current.on('animationStarted', (main) => {
+//         removeActive();
+//         const next = main.animator.targetIdx || 0;
+//         addActive(main.track.absToRel(next));
+//         slider.moveToIdx(Math.min(slider.track.details.maxIdx, next));
+//       });
+//     });
+//   };
+// }
 
 export function Index() {
   const { data, error } = useSWR('/api/chapters', fetcher);
 
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-  });
-  const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
-    {
-      initial: 0,
-      slides: {
-        perView: 4,
-        spacing: 15,
-      },
-      breakpoints: {
-        '(min-width: 481px)': {
-          slides: {
-            perView: 4,
-            spacing: 15,
-          },
-        },
-        '(min-width: 768px)': {
-          slides: {
-            perView: 6,
-            spacing: 15,
-          },
-        },
-        '(min-width: 992px)': {
-          slides: {
-            perView: 8,
-            spacing: 15,
-          },
-        },
-        '(min-width: 1024px)': {
-          slides: { perView: 10, spacing: 15 },
-        },
-        '(min-width: 1280px)': {
-          slides: { perView: 12, spacing: 15 },
-        },
-      },
-    },
-    [ThumbnailPlugin(instanceRef)]
-  );
+  // const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+  //   initial: 0,
+  // });
+  // const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
+  //   {
+  //     initial: 0,
+  //     slides: {
+  //       perView: 4,
+  //       spacing: 15,
+  //     },
+  //     breakpoints: {
+  //       '(min-width: 481px)': {
+  //         slides: {
+  //           perView: 4,
+  //           spacing: 15,
+  //         },
+  //       },
+  //       '(min-width: 768px)': {
+  //         slides: {
+  //           perView: 6,
+  //           spacing: 15,
+  //         },
+  //       },
+  //       '(min-width: 992px)': {
+  //         slides: {
+  //           perView: 8,
+  //           spacing: 15,
+  //         },
+  //       },
+  //       '(min-width: 1024px)': {
+  //         slides: { perView: 10, spacing: 15 },
+  //       },
+  //       '(min-width: 1280px)': {
+  //         slides: { perView: 12, spacing: 15 },
+  //       },
+  //     },
+  //   },
+  //   [ThumbnailPlugin(instanceRef)]
+  // );
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
@@ -105,7 +105,7 @@ export function Index() {
             <div className="text-white font-bold text-xl">Contact</div>
           </div>
         </nav> */}
-
+        {/* 
         <div ref={sliderRef} className="keen-slider text-white">
           {data.map((chapter, index) => (
             <div
@@ -130,7 +130,7 @@ export function Index() {
               <Image src={chapter.image} alt="Hero" layout="fill" />
             </div>
           ))}
-        </div>
+        </div> */}
 
         {/* <div className="relative w-auto h-auto md:w-full">
           <Image src={Hero} alt="daoism" className="rounded-lg" />
