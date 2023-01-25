44398f62abe9b2e5d96fa3681149cf4afccc02fd
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-01-24 18:24:08 -0800

    Added hotjar to RSHOGANCOM.

--- apps/rshogancom/pages/_app.tsx
+++ apps/rshogancom/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { GoogleTagManager } from '@with-nx/analytics';
+import { hotjar } from 'react-hotjar';
 
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3336749, 6);
+  }, []);
   return (
     <>
       <Component {...pageProps} />
