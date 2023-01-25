bed8842a01d5a9e29f28d10c1b0ac469e086020b
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-01-24 19:30:19 -0800

    Added pixels to RSHOGANCOM.

--- apps/rshogancom/pages/_app.tsx
+++ apps/rshogancom/pages/_app.tsx
@@ -2,13 +2,26 @@ import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { GoogleTagManager } from '@with-nx/analytics';
 import { hotjar } from 'react-hotjar';
+import { useRouter } from 'next/router';
 
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   useEffect(() => {
     hotjar.initialize(3336749, 6);
-  }, []);
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('975859380015760'); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <>
       <Component {...pageProps} />
