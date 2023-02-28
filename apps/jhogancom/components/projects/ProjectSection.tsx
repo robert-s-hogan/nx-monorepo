88be8f50799c782b9291aa806ae8931845f522db
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-02-27 16:37:34 -0800

    Added loading spinner for jhogancom.

--- apps/jhogancom/components/projects/ProjectSection.tsx
+++ apps/jhogancom/components/projects/ProjectSection.tsx
@@ -19,7 +19,31 @@ const ProjectSection = () => {
   }
 
   if (!data) {
-    return <div>Loading projects...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm12 0a8 8 0 100-16v3a5 5 0 010 10v3a8 8 0 008 8h4c0-6.627-5.373-12-12-12z"
+          ></path>
+        </svg>
+        <span>Loading projects...</span>
+      </div>
+    );
   }
 
   return (
