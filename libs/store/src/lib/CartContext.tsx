99092a533981cb7662a38ddf9abb8b88bc8862ad
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-04-13 19:55:09 -0700

    STORE: added react context e commerce

---
+++ libs/store/src/lib/CartContext.tsx
@@ -0,0 +1,102 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+// Define the Cart Item interface
+interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+// Define the cart state interface
+interface CartState {
+  cartItems: CartItem[];
+}
+
+// Define the cart actions
+type CartAction =
+  | { type: 'ADD_ITEM'; payload: CartItem }
+  | { type: 'REMOVE_ITEM'; payload: string }
+  | { type: 'UPDATE_ITEM'; payload: CartItem }
+  | { type: 'CLEAR_CART' };
+
+const initialState: CartState = {
+  cartItems: [],
+};
+
+// Create the Cart Context
+const CartContext = createContext<{
+  state: CartState;
+  dispatch: React.Dispatch<CartAction>;
+}>({ state: initialState, dispatch: () => null });
+
+// Create the reducer function
+function cartReducer(state: CartState, action: CartAction): CartState {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      // Check if the item already exists in the cart
+      const itemExists = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemExists) {
+        // If the item exists, update its quantity
+        return {
+          ...state,
+          cartItems: state.cartItems.map((item) =>
+            item.id === action.payload.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      } else {
+        // If the item doesn't exist, add it to the cart
+        return {
+          ...state,
+          cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
+        };
+      }
+
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
+      };
+
+    case 'UPDATE_ITEM':
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) =>
+          item.id === action.payload.id ? action.payload : item
+        ),
+      };
+
+    case 'CLEAR_CART':
+      return {
+        ...state,
+        cartItems: [],
+      };
+
+    default:
+      return state;
+  }
+}
+
+// Create the CartProvider component
+export const CartProvider: React.FC = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+// Custom hook to use the Cart Context
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (context === undefined) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
