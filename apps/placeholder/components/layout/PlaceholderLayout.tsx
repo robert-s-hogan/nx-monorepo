f14de76da7c6a3432ba8407de05bf8ce391eb70e
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-04-16 07:48:47 -0700

    PLACEHOLDER: final implemenation of SEO.

--- apps/placeholder/components/layout/PlaceholderLayout.tsx
+++ apps/placeholder/components/layout/PlaceholderLayout.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { PageLayout } from '@with-nx/react-ui';
+import { Seo } from '@with-nx/nextjs-react-ui';
 
 import PlaceholderHeader from './PlaceholderHeader';
 
@@ -29,22 +30,21 @@ const PlaceholderLayout: React.FC<PlaceholderLayoutProps> = ({
       header={hideNavBarProp ? '' : <PlaceholderHeader />}
       footer={<PlaceholderFooter />}
     >
-      <Head>
-        <title>{`${title ? `${title} | Placeholder` : 'Placeholder'}`}</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
-        <link rel="icon" type="image/png" href="/favicon.ico" />
-        <meta
-          name="description"
-          content={
-            description
-              ? description
-              : 'Hi, I am Jessica. Data Scientist and Data Analyst.  I want to help you make the best data-driven decisions and translate data into actionable insights.'
-          }
-        />
-      </Head>
+      <Seo
+        title={title ? title : 'Home | SWAPI'}
+        description={
+          description
+            ? description
+            : 'Explore an engaging site that consumes the https://swapi.dev/ API, showcasing data from the Star Wars universe in a visually appealing and interactive way. Learn how React and react-query are used to fetch data and implement pagination for a seamless user experience.'
+        }
+        url="https://swapi-nx.vercel.app/"
+        faviconPath="/favicon.ico"
+        image="/images/portrait.jpg"
+        twitterHandle="@robert_s_hogan"
+        siteName="Robert Hogan's SWAPI Project"
+        appleTouchIconPath="/apple-touch-icon.png"
+        keywords="swapi.dev, API, Star Wars, React, react-query, pagination, consuming endpoints, interactive, character cards, web development, API integration."
+      />
       <main
         role="main"
         className="max-w-7xl container mx-auto h-full flex-grow overflow-auto"
