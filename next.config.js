41cd9e059447015e62b44a67a1819f76478b4c4a
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-04-23 14:10:45 -0700

    PACKAGES: removed unused libraries.

---
+++ next.config.js
@@ -0,0 +1,35 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withNx } = require('@nrwl/next/plugins/with-nx');
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+  images: {
+    domains: [
+      'localhost',
+      'rshogan.imgix.net',
+      'images.pexels.com',
+      'images.unsplash.com',
+      'img.icons8.com',
+    ],
+  },
+};
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+// Merge MDX config with Next.js config
+module.exports = withNx(withMDX(nextConfig));
