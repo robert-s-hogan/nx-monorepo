d32357f64aeaf84523edb554fc3ea0de6e7d2dbd
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-04-23 19:17:42 -0700

    RSHOGANCOM: added _document.tsx file with updated _app.tsx

--- apps/rshogancom/pages/_app.tsx
+++ apps/rshogancom/pages/_app.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import { AppProps } from 'next/app';
-import { GoogleTagManager } from '@with-nx/analytics';
-import { hotjar } from 'react-hotjar';
 import { useRouter } from 'next/router';
 
 import './styles.css';
@@ -10,24 +8,14 @@ function CustomApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    hotjar.initialize(3336749, 6);
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('975859380015760');
-        ReactPixel.pageView();
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView();
-        });
-      });
+    router.events.on('routeChangeComplete', () => {
+      if (window.fbq) {
+        window.fbq('track', 'PageView');
+      }
+    });
   }, [router.events]);
-  return (
-    <>
-      <Component {...pageProps} />
-      <GoogleTagManager />
-    </>
-  );
+
+  return <Component {...pageProps} />;
 }
 
 export default CustomApp;
---
+++ apps/rshogancom/pages/_document.tsx
@@ -0,0 +1,75 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Analytics */}
+          <script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            async
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
+            }}
+          />
+
+          {/* Facebook Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '975859380015760');
+              fbq('track', 'PageView');
+            `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=975859380015760&ev=PageView&noscript=1`}
+            />
+          </noscript>
+
+          {/* Hotjar */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3336749,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
