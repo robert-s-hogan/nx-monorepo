5243e9b321c914d477163d26e34c734a4a960636
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-04-23 18:16:34 -0700

    RSHOGANCOM: reverted script changes.

--- apps/rshogancom/pages/_app.tsx
+++ apps/rshogancom/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { GoogleTagManager } from '@with-nx/analytics';
+import { hotjar } from 'react-hotjar';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
 
 import './styles.css';
 
@@ -9,45 +10,22 @@ function CustomApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    router.events.on('routeChangeComplete', () => {
-      if (window.fbq) {
-        window.fbq('track', 'PageView');
-      }
-    });
-  }, [router.events]);
+    hotjar.initialize(3336749, 6);
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('975859380015760');
+        ReactPixel.pageView();
 
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <>
       <Component {...pageProps} />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        strategy="lazyOnload"
-        onLoad={() => {
-          window.dataLayer = window.dataLayer || [];
-          function gtag(...args: any[]) {
-            window.dataLayer.push(args);
-          }
-          gtag('js', new Date());
-          gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
-        }}
-      />
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() => {
-          window.fbq =
-            window.fbq ||
-            function () {
-              (window.fbq.q = window.fbq.q || []).push(arguments);
-            };
-          window.fbq('init', '975859380015760');
-          window.fbq('track', 'PageView');
-        }}
-      />
-      <Script
-        src="https://static.hotjar.com/c/hotjar-3336749.js?sv=6"
-        strategy="lazyOnload"
-      />
+      <GoogleTagManager />
     </>
   );
 }
