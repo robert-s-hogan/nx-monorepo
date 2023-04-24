83c48c97d983b0a9e6a0f6150f8cdd8995fd9071
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-04-24 04:25:41 -0700

    RSHOGAN & LIBS: cleaned up libs and fixed crossOrigin=anpnymous

--- apps/rshogancom/pages/_document.tsx
+++ apps/rshogancom/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com/"
-            crossOrigin
+            crossOrigin="anonymous"
           />
 
           {/* Google Analytics */}
