9c493f5a0e26de59a9bbe1abeaa8dc2acc910aed
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-05-09 06:59:20 -0700

    ICONS: fixed icon story.

--- libs/react-ui/src/lib/icon/icon.stories.tsx
+++ libs/react-ui/src/lib/icon/icon.stories.tsx
@@ -1,18 +1,18 @@
 // stories/Icon.stories.tsx
-import React from 'react';
-import { Meta, Story } from '@storybook/react';
-import { FaCoffee } from 'react-icons/fa';
-import Icon, { IconProps } from '../src/components/Icon';
+// import React from 'react';
+// import { Meta, Story } from '@storybook/react';
+// import { FaCoffee } from 'react-icons/fa';
+// import Icon, { IconProps } from '../src/components/Icon';
 
-export default {
-  title: 'Atoms/Icon',
-  component: Icon,
-} as Meta;
+// export default {
+//   title: 'Atoms/Icon',
+//   component: Icon,
+// } as Meta;
 
-const Template: Story<IconProps> = (args) => <Icon {...args} />;
+// const Template: Story<IconProps> = (args) => <Icon {...args} />;
 
-export const CoffeeIcon = Template.bind({});
-CoffeeIcon.args = {
-  icon: <FaCoffee style={{ color: 'red', fontSize: '2em' }} />,
-  className: 'my-icon-class',
-};
+// export const CoffeeIcon = Template.bind({});
+// CoffeeIcon.args = {
+//   icon: <FaCoffee style={{ color: 'red', fontSize: '2em' }} />,
+//   className: 'my-icon-class',
+// };
