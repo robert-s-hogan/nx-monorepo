85f3ade13aeba20ff394054ec9ffed0175451e95
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-05-14 14:15:10 -0700

    REACT-UI: updated box story.

--- libs/react-ui/src/lib/box/box.stories.tsx
+++ libs/react-ui/src/lib/box/box.stories.tsx
@@ -3,17 +3,40 @@ import { Box, BoxProps } from './box';
 
 const meta: Meta = {
   component: Box,
-  title: 'Box',
   title: 'Atoms/Box',
   argTypes: {
-    id: { control: 'text' },
-    children: { control: 'text' },
-    className: { control: 'text' },
-    role: { control: 'text' },
-    onClick: { action: 'clicked' },
-    style: { control: 'object' },
+    children: {
+      control: 'text',
+      description: 'React.ReactNode',
+      defaultValue: '',
+    },
+    id: {
+      control: 'text',
+      description: 'string',
+      defaultValue: '',
+    },
+    className: {
+      control: 'text',
+      description: 'string',
+      defaultValue: '',
+    },
+    role: {
+      control: 'text',
+      description: 'string',
+      defaultValue: '',
+    },
+    style: {
+      control: 'object',
+      description: 'React.CSSProperties',
+      defaultValue: '',
+    },
+    onClick: {
+      action: 'clicked',
+      description: 'React.MouseEventHandler<HTMLDivElement>',
+    },
   },
 };
+
 export default meta;
 
 const Template: Story<BoxProps> = (args) => <Box {...args} />;
@@ -30,19 +53,33 @@ Secondary.args = {
   className: 'bg-gray-300 text-black p-4 rounded',
 };
 
-export const WithCustomStyle = Template.bind({});
-WithCustomStyle.args = {
+export const WithRole = Template.bind({});
+WithRole.args = {
+  children: 'Box with Role',
+  role: 'presentation',
+  className: 'bg-green-500 text-white p-4 rounded',
+};
+
+export const WithId = Template.bind({});
+WithId.args = {
+  id: 'customBoxId',
+  children: 'Box with Id',
+  className: 'bg-purple-500 text-white p-4 rounded',
+};
+
+export const WithOnClick = Template.bind({});
+WithOnClick.args = {
+  children: 'Clickable Box',
+  className: 'bg-yellow-500 text-black p-4 rounded cursor-pointer',
+  onClick: () => {},
+};
+
+export const WithStyle = Template.bind({});
+WithStyle.args = {
   children: 'Styled Box',
   className: 'p-4 rounded',
   style: {
-    backgroundColor: 'purple',
-    color: 'white',
+    backgroundColor: 'cyan',
+    color: 'black',
   },
-};
-
-export const Clickable = Template.bind({});
-Clickable.args = {
-  children: 'Clickable Box',
-  className: 'bg-green-500 text-white p-4 rounded cursor-pointer',
-  role: 'button',
 };
