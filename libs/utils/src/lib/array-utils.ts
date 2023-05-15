1987f7915d6b4801c19a858152b9bf3efa5d4696
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-05-15 11:48:50 -0700

    UTILS: commented out functions that didn't build.

--- libs/utils/src/lib/array-utils.ts
+++ libs/utils/src/lib/array-utils.ts
@@ -4,30 +4,34 @@ export function getRandomElement<T>(array: T[]): T {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-export const sortProducts = (
-  products: Product[],
-  criteria: SortCriteria
-): Product[] => {
-  return products.sort((a, b) => {
-    if (criteria === 'price') {
-      return a.price - b.price;
-    } else if (criteria === 'rating') {
-      return b.rating - a.rating;
-    }
-    return 0;
-  });
-};
+// export const sortProducts = (
+//   products: Product[],
+//   criteria: SortCriteria
+// ): Product[] => {
+//   return products.sort((a, b) => {
+//     if (criteria === 'price') {
+//       return a.price - b.price;
+//     } else if (criteria === 'rating') {
+//       return b.rating - a.rating;
+//     }
+//     return 0;
+//   });
+// };
 
-export const removeFalsyValuesFromArray<T>(arr: T[]): T[] {
-  return arr.filter(Boolean);
-}
+// export const removeFalsyValuesFromArray<T>(arr: T[]): T[] {
+//   return arr.filter(Boolean);
+// }
 
-export function cleanObject<T extends Record<string, any>>(obj: T): Partial<T> {
-  const result: Partial<T> = {};
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key) && obj[key] !== null && obj[key] !== undefined) {
-      result[key] = obj[key];
-    }
-  }
-  return result;
-}
+// export function cleanObject<T extends Record<string, any>>(obj: T): Partial<T> {
+//   const result: Partial<T> = {};
+//   for (const key in obj) {
+//     if (
+//       Object.prototype.hasOwnProperty.call(obj, key) &&
+//       obj[key] !== null &&
+//       obj[key] !== undefined
+//     ) {
+//       result[key] = obj[key];
+//     }
+//   }
+//   return result;
+// }
--- libs/utils/src/lib/format-utils.ts
+++ libs/utils/src/lib/format-utils.ts
@@ -6,14 +6,14 @@ export const formatCurrency = (amount: number, currency = 'USD'): string => {
   }).format(amount);
 };
 
-function formatDate(
-  date: Date | string,
-  format: string = 'YYYY-MM-DD'
-): string {
-  const dateObj = typeof date === 'string' ? new Date(date) : date;
-  const year = dateObj.getFullYear();
-  const month = String(dateObj.getMonth() + 1).padStart(2, '0');
-  const day = String(dateObj.getDate()).padStart(2, '0');
+// function formatDate(
+//   date: Date | string,
+//   format: string = 'YYYY-MM-DD'
+// ): string {
+//   const dateObj = typeof date === 'string' ? new Date(date) : date;
+//   const year = dateObj.getFullYear();
+//   const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+//   const day = String(dateObj.getDate()).padStart(2, '0');
 
-  return format.replace('YYYY', year).replace('MM', month).replace('DD', day);
-}
+//   return format.replace('YYYY', year).replace('MM', month).replace('DD', day);
+// }
