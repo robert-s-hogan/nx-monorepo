49e7afb2fafba1f34f4365981bf803c551c8d777
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-05-20 14:20:19 -0700

    Added organized structure .mdx to react-ui for planning.

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,11 +1,11 @@
-export * from './lib/control-types'; //button, accordion, tabs, pagination, progressbar, slider, rating, spinner
+export * from './lib/control-types'; //button, accordion, tabs, link, pagination, progressbar, slider, rating, spinner
 export * from './lib/feedback-types'; //toast, alert, spinner, skeleton
-export * from './lib/display-types'; //card, image, video, carousel, modal, tooltip, popover
-export * from './lib/form-types'; //form, input, textarea, checkbox, radio, select
+export * from './lib/display-types'; //card, modal, tooltip, popover
+export * from './lib/form-types'; //form, input, textarea, checkbox, radio
 export * from './lib/layout-types'; //page-layout, section, grid, contianer, flex
 export * from './lib/media-types'; //image, video, audio, carousel, gallery
-export * from './lib/navigation-types'; //navbar, link, button, searchbar, select
+export * from './lib/navigation-types'; //navbar, link, searchbar, select
 export * from './lib/redux-toolkit-types'; //slice, store, thunk, action, reducer
 export * from './lib/serverless-function-types'; //chillout
-export * from './lib/typography-types'; //heading, text, paragraph, list, blockquote, code, link
+export * from './lib/typography-types'; //heading, text, paragraph, list, blockquote, code
 export * from './lib/utility-types'; //seo
