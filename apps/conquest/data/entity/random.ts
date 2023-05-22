793daedca5699ef0de4b0b784a10f6d8966e324d
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-05-21 18:23:04 -0700

    REMOVED conquest => on conquest-api branch

--- apps/conquest/data/entity/random.ts
+++
@@ -1,6 +0,0 @@
-import seedrandom from 'seedrandom';
-
-export const random = (arr: any[], seed?: number): any => {
-  const rng = seed ? seedrandom(seed) : Math.random;
-  return arr[Math.floor(rng() * arr.length)];
-};
--- apps/conquest/pages/api/caravan.ts
+++
@@ -1,5 +0,0 @@
-import { caravan } from '@with-nx/constants';
-
-export default function handler(req, res) {
-  res.status(200).json(caravan);
-}
