7b4b823c1a066079c7ce7feefab782c12c2ead10
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-05-27 19:40:42 -0700

    JHOGANCOM: updated images.

--- apps/jhogancom/data/projects.ts
+++ apps/jhogancom/data/projects.ts
@@ -6,7 +6,8 @@ export const projectStaticData = [
     description:
       'For this project, I used Udemy class data from Kaggle.com to try and predict which classes are successful using Pandas, matplotlib and scikit-learn.',
     link: 'https://github.com/jessicahoganma/machine_learning_udemy_class_data',
-    image: '/images/machine-learning-project.jpg',
+    image:
+      'https://cloudflare-assets-nx-monorepo.vercel.app/jhogancom/machine-learning-project.jpg',
     featured: true,
     order: 1,
   },
@@ -17,7 +18,8 @@ export const projectStaticData = [
     description:
       'In this project I used various resources to clean and merge datasets from Google Analytics as well as create visualizations, analyze timeseries and build alerts for for marketing activities. ',
     link: 'https://github.com/jessicahoganma/Python-For-Marketing',
-    image: '/images/python-for-marketing.webp',
+    image:
+      'https://cloudflare-assets-nx-monorepo.vercel.app/jhogancom/python-for-marketing.webp',
     featured: true,
     order: 2,
   },
@@ -28,7 +30,8 @@ export const projectStaticData = [
     description:
       'My master’s thesis is a quantitative research project that measured California residents’ attitudes towards parenthood using SPSS. ',
     link: 'https://csu-csus.esploro.exlibrisgroup.com/esploro/outputs/graduate/Parental-status-and-life-satisfaction-a/99257831096601671',
-    image: '/images/masters-thesis.webp',
+    image:
+      'https://cloudflare-assets-nx-monorepo.vercel.app/jhogancom/masters-thesis.webp',
   },
 
   {
@@ -38,7 +41,8 @@ export const projectStaticData = [
     description:
       'For this project I used the MySQL Connector, Python and pandas to implement a database on MySQL Server to create, read, update and delete data. ',
     link: 'https://github.com/jessicahoganma/Python_SQL/blob/main/README.md',
-    image: '/images/python-project.jpg',
+    image:
+      'https://cloudflare-assets-nx-monorepo.vercel.app/jhogancom/python-project.jpg',
     featured: true,
     order: 3,
   },
@@ -49,6 +53,7 @@ export const projectStaticData = [
     description:
       'I developed a relational database from scratch through MYSQL Community Server. I used Lucidchart (for my ERD), GitHub and PopSQL. ',
     link: 'https://github.com/jessicahoganma/SQL_hospital_Project',
-    image: '/images/sql-project.webp',
+    image:
+      'https://cloudflare-assets-nx-monorepo.vercel.app/jhogancom/sql-project.webp',
   },
 ];
--- apps/jhogancom/next.config.js
+++ apps/jhogancom/next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  images: {
+    domains: ['cloudflare-assets-nx-monorepo.vercel.app'],
+  },
 };
 
 module.exports = withNx(nextConfig);
