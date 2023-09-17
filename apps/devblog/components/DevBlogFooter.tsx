6302f4d7aab9908861cb93104381076603768bf1
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-09-17 14:01:59 -0700

    devblog: reworked footer width and padding.

--- apps/devblog/components/DevBlogFooter.tsx
+++ apps/devblog/components/DevBlogFooter.tsx
@@ -11,13 +11,13 @@ import RSHLogo from '../public/images/rsh_logo.png';
 
 const DevBlogFooter = () => {
   return (
-    <footer className="container mx-auto max-w-7xl pb-0 px-4 mt-8">
-      <Flex className="flex-row justify-between md:px-4 py-4 items-center mx-0 md:mx-12 space-y-2 md:space-y-0">
+    <footer className="container mx-auto max-w-7xl pb-0 mt-8">
+      <Flex className="flex-row justify-between py-4 items-center mx-0space-y-2 md:space-y-0">
         <Link href="/" className="button-link">
           <CustomRSHLogo className="h-12 w-12 text-primary nav-logo" />
         </Link>
 
-        <Flex className="sm:mt-0 pt-4 justify-center pb-4 lg:pb-0 space-x-4">
+        <Flex className="sm:mt-0 pt-4 justify-center pb-4 lg:pb-0 space-x-4  mr-2 md:mr-0 ">
           <Link
             href="mailto:robert@robertshogan.com"
             className=" hover:text-vivid-500"
