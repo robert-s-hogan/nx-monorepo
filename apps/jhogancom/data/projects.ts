d0dbacd0bb47da20ed202c1d07837f13fad305e3
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-10-04 15:22:08 -0700

    jhogancom: sqooshed images

--- apps/jhogancom/data/projects.ts
+++ apps/jhogancom/data/projects.ts
@@ -7,7 +7,7 @@ export const projectStaticData = [
       'For this project, I used Udemy class data from Kaggle.com to try and predict which classes are successful using Pandas, matplotlib and scikit-learn.',
     link: 'https://github.com/jessicahoganma/machine_learning_udemy_class_data',
     image:
-      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_machine_learning_project-2.png',
+      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_maching_learning_project-3.jpg',
     featured: true,
     order: 1,
   },
@@ -19,7 +19,7 @@ export const projectStaticData = [
       'In this project I used various resources to clean and merge datasets from Google Analytics as well as create visualizations, analyze timeseries and build alerts for for marketing activities. ',
     link: 'https://github.com/jessicahoganma/Python-For-Marketing',
     image:
-      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_python_for_marketing.png',
+      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_python_for_marketing.jpg',
     featured: true,
     order: 2,
   },
@@ -31,7 +31,7 @@ export const projectStaticData = [
       'My master’s thesis is a quantitative research project that measured California residents’ attitudes towards parenthood using SPSS. ',
     link: 'https://csu-csus.esploro.exlibrisgroup.com/esploro/outputs/graduate/Parental-status-and-life-satisfaction-a/99257831096601671',
     image:
-      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_masters_thesis.png',
+      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_masters_thesis.jpg',
   },
 
   {
@@ -42,7 +42,7 @@ export const projectStaticData = [
       'For this project I used the MySQL Connector, Python and pandas to implement a database on MySQL Server to create, read, update and delete data. ',
     link: 'https://github.com/jessicahoganma/Python_SQL/blob/main/README.md',
     image:
-      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_python_project.png',
+      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_python_project.jpg',
     featured: true,
     order: 3,
   },
@@ -54,6 +54,6 @@ export const projectStaticData = [
       'I developed a relational database from scratch through MYSQL Community Server. I used Lucidchart (for my ERD), GitHub and PopSQL. ',
     link: 'https://github.com/jessicahoganma/SQL_hospital_Project',
     image:
-      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_sql_project.png',
+      'https://rshogan.imgix.net/projects/jhogancom/images/jhogan_sql_project.jpg',
   },
 ];
