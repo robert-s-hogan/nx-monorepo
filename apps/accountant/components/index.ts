b8f31c8e8b0fbd9199cdef65e83e1fd4e80502f5
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2024-04-17 07:49:48 -0700

    all: removed accountant

--- apps/accountant/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AccountantLayout } from './AccountantLayout';
-export { default as AccountantHeader } from './AccountantHeader';
-export { default as AccountantFooter } from './AccountantFooter';
