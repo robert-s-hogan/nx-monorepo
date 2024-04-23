3fcbfd83a6b020532ef264384d17f29c059327c9
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2024-04-23 10:14:33 -0700

    conquest: removed hideEdit logic

--- apps/conquest/src/app/components/CampaignList.tsx
+++ apps/conquest/src/app/components/CampaignList.tsx
@@ -71,118 +71,65 @@ const CampaignList: React.FC<CampaignListProps> = ({
                 key={campaign.id}
                 className="border border-black rounded p-4"
               >
-                {hideEdit ? (
-                  <>
-                    <div className="pb-3">
-                      <div className="flex space-x-2 items-center">
-                        <Link href={`/campaigns/${campaign.slug}`}>
-                          <div className="flex space-x-2 items-center">
-                            <h2 className="">{campaign.name}</h2>
-                            <span>({campaign.numberOfPlayers})</span>
-                          </div>
-                          <span className="text-gray-400 text-sm">
-                            /{campaign.slug}
-                          </span>
-                        </Link>
-                      </div>
-                      <p>{campaign.description}</p>
-                    </div>
-                    <div className="grid grid-cols-1 gap-8">
-                      <div>
-                        <h2>XP Calculations</h2>
-                        <p>
-                          Player Experience Start:{' '}
-                          {campaign.playerExperienceStart}
-                        </p>
-                        <p>Adventuring Day XP Limit: {adventuringDayXP}</p>
+                <div className="p-4">
+                  <div className="flex justify-end pr-2 pt-2">
+                    <button onClick={() => onEdit && onEdit(campaign)}>
+                      <IonIcon icon={create} size="large" />
+                    </button>
 
-                        <ProgressBar
-                          xpStart={campaign.levelDetails?.xpStart}
-                          xpEnd={campaign.levelDetails?.xpEnd}
-                          playerExperience={campaign.playerExperienceStart}
-                          onTakeFirstShortRest={() => handleTakeShortRest(1)}
-                          onTakeSecondShortRest={() => handleTakeShortRest(2)}
-                          isShortRestAvailable={shortRestsAvailable}
-                          // onTakeLongRest={handleTakeLongRest}
-                        />
-
-                        <p>Level of Players: {levelDetails?.level}</p>
-                      </div>
-                      <div>
-                        <RestCalculationDisplay
-                          playerExperienceStart={campaign.playerExperienceStart}
-                          adventuringDayXPLimit={adventuringDayXP}
-                          xpStart={campaign.levelDetails?.xpStart}
-                          xpThresholdEasy={xpThresholdEasy}
-                          shortRestsAvailable={shortRestsAvailable}
-                          longRestNeeded={rests.longRestNeeded}
-                          // onTakeShortRest={handleTakeShortRest}
-                          // onTakeLongRest={handleTakeLongRest}
-                        />
-                      </div>
+                    <button onClick={() => onDelete(campaign.id as string)}>
+                      <IonIcon icon={trash} size="large" />
+                    </button>
+                  </div>
+                  <div className="pb-3">
+                    <div className="flex space-x-2 items-center">
+                      <h2 className="">{campaign.name}</h2>{' '}
+                      <span className="text-gray-400 text-sm">
+                        /{campaign.slug}
+                      </span>
                     </div>
-                  </>
-                ) : (
-                  <div className="p-4">
-                    <div className="flex justify-end pr-2 pt-2">
-                      <button onClick={() => onEdit && onEdit(campaign)}>
-                        <IonIcon icon={create} size="large" />
-                      </button>
-
-                      <button onClick={() => onDelete(campaign.id as string)}>
-                        <IonIcon icon={trash} size="large" />
-                      </button>
-                    </div>
-                    <div className="pb-3">
-                      <div className="flex space-x-2 items-center">
-                        <h2 className="">{campaign.name}</h2>{' '}
-                        <span className="text-gray-400 text-sm">
-                          /{campaign.slug}
-                        </span>
+                    <p>{campaign.description}</p>
+                  </div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <div />
+                    <div className="p-3 col-span-2 space-y-2 text-lg">
+                      <h2 className="py-1 bg-white">XP Calculations</h2>
+                      <p>XP Progress Bar</p>
+                      <ProgressBar
+                        xpStart={campaign.levelDetails?.xpStart}
+                        xpEnd={campaign.levelDetails?.xpEnd}
+                        playerExperience={campaign.playerExperienceStart}
+                        onTakeFirstShortRest={() => handleTakeShortRest(1)}
+                        onTakeSecondShortRest={() => handleTakeShortRest(2)}
+                        isShortRestAvailable={shortRestsAvailable}
+                        // onTakeLongRest={handleTakeLongRest}
+                      />
+                      <div className="w-full flex items-center justify-between">
+                        <p>Player Experience Start:</p>
+                        <span>{campaign.playerExperienceStart}</span>
                       </div>
-                      <p>{campaign.description}</p>
-                    </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                      <div />
-                      <div className="p-3 col-span-2 space-y-2 text-lg">
-                        <h2 className="py-1 bg-white">XP Calculations</h2>
-                        <p>XP Progress Bar</p>
-                        <ProgressBar
-                          xpStart={campaign.levelDetails?.xpStart}
-                          xpEnd={campaign.levelDetails?.xpEnd}
-                          playerExperience={campaign.playerExperienceStart}
-                          onTakeFirstShortRest={() => handleTakeShortRest(1)}
-                          onTakeSecondShortRest={() => handleTakeShortRest(2)}
-                          isShortRestAvailable={shortRestsAvailable}
-                          // onTakeLongRest={handleTakeLongRest}
-                        />
-                        <div className="w-full flex items-center justify-between">
-                          <p>Player Experience Start:</p>
-                          <span>{campaign.playerExperienceStart}</span>
-                        </div>
-                        <div className="w-full flex items-center justify-between">
-                          <p>Adventuring Day XP Limit:</p>
-                          <span>{adventuringDayXP}</span>
-                        </div>
-
-                        <div className="w-full flex items-center justify-between">
-                          <p>Level of Players:</p>
-                          <span>{levelDetails?.level}</span>
-                        </div>
-                        <RestCalculationDisplay
-                          playerExperienceStart={campaign.playerExperienceStart}
-                          adventuringDayXPLimit={adventuringDayXP}
-                          xpStart={campaign.levelDetails?.xpStart}
-                          xpThresholdEasy={xpThresholdEasy}
-                          shortRestsAvailable={shortRestsAvailable}
-                          longRestNeeded={rests.longRestNeeded}
-                          // onTakeShortRest={handleTakeShortRest}
-                          // onTakeLongRest={handleTakeLongRest}
-                        />
+                      <div className="w-full flex items-center justify-between">
+                        <p>Adventuring Day XP Limit:</p>
+                        <span>{adventuringDayXP}</span>
                       </div>
+
+                      <div className="w-full flex items-center justify-between">
+                        <p>Level of Players:</p>
+                        <span>{levelDetails?.level}</span>
+                      </div>
+                      <RestCalculationDisplay
+                        playerExperienceStart={campaign.playerExperienceStart}
+                        adventuringDayXPLimit={adventuringDayXP}
+                        xpStart={campaign.levelDetails?.xpStart}
+                        xpThresholdEasy={xpThresholdEasy}
+                        shortRestsAvailable={shortRestsAvailable}
+                        longRestNeeded={rests.longRestNeeded}
+                        // onTakeShortRest={handleTakeShortRest}
+                        // onTakeLongRest={handleTakeLongRest}
+                      />
                     </div>
                   </div>
-                )}
+                </div>
               </div>
             );
           })}
